4c05954209d8c2b87cbd727a17f68de3c6f3bfd2
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-03-15 22:05:48 -0600

    Refactor movie params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,11 +14,6 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-
-    movie_params =
-      params.require(:movie).
-        permit(:title, :description, :rating, :released_on, :total_gross)
-
     @movie.update(movie_params)
     redirect_to @movie
   end
@@ -28,14 +23,20 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie_params =
-      params.require(:movie).
-        permit(:title, :description, :rating, :released_on, :total_gross)
-
     @movie = Movie.new(movie_params)
     @movie.save
     redirect_to @movie
 
   end
 
+
+
+private
+
+  def movie_params
+      params.require(:movie).
+        permit(:title, :description, :rating, :released_on, :total_gross)
+  end
+
 end
+
