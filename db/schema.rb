103a0697be425d76bea875dd8ad7a9ff0e23ff0f
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-03-20 16:16:04 -0600

    Add methods to movie class & new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_181835) do
+ActiveRecord::Schema.define(version: 2022_03_20_220023) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2022_02_10_181835) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.date "released_on"
+    t.string "director"
+    t.string "duration"
+    t.string "image_file_name", default: "placeholder.png"
   end
 
 end
