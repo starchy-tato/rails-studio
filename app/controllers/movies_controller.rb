48a9d641e654fac3a580f2bfbcc92c29af0eaa3c
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-03-20 16:30:22 -0600

    Include new params & update show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -39,7 +39,7 @@ private
 
   def movie_params
       params.require(:movie).
-        permit(:title, :description, :rating, :released_on, :total_gross)
+        permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration, :image_file_name)
   end
 
 end
