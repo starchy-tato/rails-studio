71ebb3415ae20c040c11625e80a7f121fcc1dcd0
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-05-03 20:55:00 -0600

    add review create functionality
    * add create action
    * create a view for a new review
    * update movie show page with a button for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,9 @@ class ReviewsController < ApplicationController
     @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new
+  end
 end
