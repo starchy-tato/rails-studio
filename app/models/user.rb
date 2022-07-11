58c870d3aa791a77717acb6090a85b436dc3a3f5
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-07-10 20:44:16 -0600

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, format: { with: /\S+@\S+/ },
             uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 5, allow_blank: true }
 end
