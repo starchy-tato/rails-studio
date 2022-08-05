ac2f8e49ae4717430f77d40320e20444cb073af0
Tiffanie Wong <tiffanie@pixeltree.ca>
2022-08-05 15:02:36 -0600

    create new user account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "Thank you for signing up!"
+    else
+      render :new
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user)
+          .permit(:name, :email, :password, :password_confirmation)
+  end
+
+
 end
