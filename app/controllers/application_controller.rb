55df32216137a407c5dc1e17eff43a4d36808981
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-01-18 22:04:52 -0700

    move current_user to app controller
    * update method to be a helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id])if session[:user_id]
+  end
 
+  helper_method :current_user
 end
