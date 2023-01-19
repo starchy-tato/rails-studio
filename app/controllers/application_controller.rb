6e225ae0a24d00c3f6a2bdd88a820a8036c05447
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-01-18 22:00:18 -0700

    add require_signin method
    * adding to application_controller to allow all controllers access to this method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   add_flash_types(:danger)
+
+  private
+
+  def require_signin
+    unless current_user
+      redirect_to new_session_url, alert: "Please sign in first!"
+    end
+  end
+
+
 end
