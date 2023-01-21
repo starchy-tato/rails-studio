bc164982b8d3d9d392c1e44e7e0f0b932ed5cc54
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-01-20 21:23:58 -0700

    create new method to check user is correct
    * add new method as before_action
    * refactor edit update and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -25,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account successfully updated!"
     else
@@ -38,7 +37,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to movies_url, alert: "Account has been deleted =')"
@@ -51,5 +49,11 @@ private
           .permit(:name, :email, :password, :password_confirmation, :username)
   end
 
+  def require_correct_user
+    @user = User.find(params[:id])
+    unless current_user == @user
+      redirect_to root_url
+    end
+  end
 
 end
