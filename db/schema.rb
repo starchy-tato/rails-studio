e9af79d72f94aa97c768669c6c4a80dc9c9b82ec
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-01-22 14:43:46 -0700

    add boolean admin field to user table
    * migration with a default of false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_02_234658) do
+ActiveRecord::Schema.define(version: 2023_01_22_213737) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2022_12_02_234658) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "reviews", "movies"
