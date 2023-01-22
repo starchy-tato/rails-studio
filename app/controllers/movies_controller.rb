003eff0e0f407374a8387dae06159014884b4c5f
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-01-22 15:15:36 -0700

    update to allow only admins to delete accounts
    * move admin methods to application controller for accessibility

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,18 +3,6 @@ class MoviesController < ApplicationController
   before_action :require_signin, except: [:index, :show]
   before_action :require_admin, except: [:index, :show]
 
-  def require_admin
-    unless current_user_admin?
-      redirect_to root_url, alert: "Unauthorized access =("
-    end
-  end
-
-  def current_user_admin?
-    current_user&.admin?
-  end
-
-  helper_method :current_user_admin?
-
   def index
     @movies = Movie.released
   end
