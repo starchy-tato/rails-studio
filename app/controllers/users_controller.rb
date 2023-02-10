68ad478071cf68f2eefe94157c0f942833eb7967
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-02-09 23:30:09 -0700

    updating user show page to display fav movies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews
+    @favourite_movies = @user.favourite_movies
   end
 
   def new
