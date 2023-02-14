22e1067cae991e1a623ab09cce429c8e4a8a3626
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-02-13 21:41:04 -0700

    create fav create action

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,9 @@
 class FavouritesController < ApplicationController
+  before_action :require_signin
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @movie.favourites.create!(user: current_user)
+    redirect_to @movie
+  end
 end
