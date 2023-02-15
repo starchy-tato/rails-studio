0954d8aa989507ecc35645b169545ea0cee152a9
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-02-14 23:36:01 -0700

    refactor setting movie id into method

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,8 +1,8 @@
 class FavouritesController < ApplicationController
   before_action :require_signin
+  before_action :set_movie
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @movie.favourites.create!(user: current_user)
     redirect_to @movie
   end
@@ -10,8 +10,12 @@ class FavouritesController < ApplicationController
   def destroy
     favourite = current_user.favourites.find(params[:id])
     favourite.destroy
+    redirect_to @movie
+  end
 
-    movie = Movie.find(params[:movie_id])
-    redirect_to movie
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
