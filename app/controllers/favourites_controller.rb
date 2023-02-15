80ae8067cc97e0c597046477506368c878bd8cde
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-02-14 23:14:46 -0700

    move fav button logic to helper file
    * general formatting

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -10,7 +10,7 @@ class FavouritesController < ApplicationController
   def destroy
     favourite = current_user.favourites.find(params[:id])
     favourite.destroy
-    
+
     movie = Movie.find(params[:movie_id])
     redirect_to movie
   end
