759fb0ac207fba527c9fed7c1ab42188ddd7d4d5
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-02-19 15:37:55 -0700

    update released method to scope

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -18,12 +18,10 @@ class Movie < ApplicationRecord
   RATINGS = %w(G PG PG-13 R NC-17)
   validates :rating, inclusion: { in: RATINGS}
 
+  scope :released, -> { (where("released_on < ?", Time.now).order("released_on desc")) }
+
   def flop?
     total_gross.blank? || total_gross < 225_000_000
-  end
-
-  def self.released
-    where("released_on < ?", Time.now).order("released_on desc")
   end
 
   def self.hits
