907859ea91ebcffb020ebf29579856a327ebc47e
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-02-19 17:57:34 -0700

    add recent scope

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,6 +20,7 @@ class Movie < ApplicationRecord
 
   scope :released, -> { where("released_on < ?", Time.current).order("released_on desc") }
   scope :upcoming, -> { where("released_on > ?", Time.current).order("released_on asc") }
+  scope :recent, -> (max = 5) { released.limit(max) }
 
   def flop?
     total_gross.blank? || total_gross < 225_000_000
