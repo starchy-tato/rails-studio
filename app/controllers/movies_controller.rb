2d8fbd89eeda746ec5123a4f455a997584c62dc3
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-02-20 22:24:23 -0700

    add custom route for filters
    * update movie index action with cases for filtering
    * add deadpool 3 asset

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,14 @@ class MoviesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
 
   def index
-    @movies = Movie.released
+    case params[:filter]
+    when "upcoming"
+      @movies = Movie.upcoming
+    when "recent"
+      @movies = Movie.recent
+    else
+      @movies = Movie.released
+    end
   end
 
   def show
