0ba9d78ff26004792ed700a00272ac17671bc290
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-02-22 20:14:47 -0700

    update index action
    * add movie filter method to check for approved scope filter params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,18 +4,7 @@ class MoviesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
 
   def index
-    case params[:filter]
-    when "upcoming"
-      @movies = Movie.upcoming
-    when "recent"
-      @movies = Movie.recent
-    when "hits"
-      @movies = Movie.hits
-    when "flops"
-      @movies = Movie.flops
-    else
-      @movies = Movie.released
-    end
+    @movies = Movie.send(movies_filter)
   end
 
   def show
@@ -67,6 +56,14 @@ private
         permit(:title, :description, :rating, :released_on,
                :total_gross, :director, :duration, :image_file_name,
                genre_ids: [])
+  end
+
+  def movies_filter
+    if params[:filter].in? %w(upcoming recent hits flops)
+      params[:filter]
+    else
+      :released
+    end
   end
 end
 
