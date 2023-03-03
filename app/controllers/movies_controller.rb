54f54b9154ac7022e848f5d38e7f64cd7646a0c1
Tiffanie Wong <tiffanie@pixeltree.ca>
2023-03-03 16:11:41 -0700

    refactor movie actions to find movie via slug
    * add before action hook to remove duplication

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,13 +2,13 @@ class MoviesController < ApplicationController
 
   before_action :require_signin, except: [:index, :show]
   before_action :require_admin, except: [:index, :show]
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
   def index
     @movies = Movie.send(movies_filter)
   end
 
   def show
-    @movie = Movie.find(params[:id])
     @fans = @movie.fans
     @genres = @movie.genres.order(:name)
     if current_user
@@ -17,11 +17,9 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params[:id])
   end
 
   def update
-    @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie successfully updated!"
     else
@@ -44,7 +42,6 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
     @movie.destroy
     redirect_to movies_url, alert: "Movie has been deleted"
   end
@@ -64,6 +61,10 @@ private
     else
       :released
     end
+  end
+
+  def set_movie
+    @movie = Movie.find_by!(slug: params[:id])
   end
 end
 
